70d54cbcb3296a1869fb6bef6da439de1e7eff4c
harshbajani <harshbajani7@gmail.com>
2025-05-09 10:31:43 +0530

    dashboard layout

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
+import { Header } from "components";
 import React from "react";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Title Page"
+        description="Checkout our current users in real time"
+      />
+    </main>
+  );
 };
 
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Header } from "components";
+
 const Dashboard = () => {
-  return <div>Dashboard page</div>;
+  const user = { name: "harsh" };
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity trends and popular destinations in real time"
+      />
+    </main>
+  );
 };
 
 export default Dashboard;
